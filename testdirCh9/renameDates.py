3cec0ef3f55b1164d72bee5bb9d953150e745fa3
Greg Janowiak <gregory.janowiak@gmail.com>
2017-08-05 18:12:41 -0700

    renameDates.py edits

--- testdirCh9/renameDates.py
+++ testdirCh9/renameDates.py
@@ -1,35 +1,40 @@
-#! /usr/bin/env python3
+#! python3
+# renameDates.py - Renames filenames with American MM-DD-YYYY date format
+# to European DD-MM-YYYY.
+
 import shutil, os, re
 
-# create a regex to match with american date format
-datePattern = re.compile(r'''
-    ^(.*?)  # all text before the date
+# Create a regex that matches files with the American date format.
+datePattern = re.compile(r"""^(.*?) # all text before the date
     ((0|1)?\d)- # one or two digits for the month
-    ((0|1|2|3)?\d)  # one or two digits for the day
-    ((19|20)\d\d)   # four digits for year, starting with 19 or 20
-    (.*?)$  # all text after the date
-''', re.VERBOSE)
+    ((0|1|2|3)?\d)- # one or two digits for the day
+    ((19|20)\d\d) # four digits for the year (must start with 19 or 20)
+    (.*?)$ # all text after the date
+    """, re.VERBOSE)
 
-# loop through files in working directory
+# Loop over the files in the working directory.
 for amerFilename in os.listdir('.'):
     mo = datePattern.search(amerFilename)
 
+    # Skip files without a date.
     if mo == None:
         continue
 
+    # Get the different parts of the filename.
     beforePart = mo.group(1)
-    monthPart = mo.group(2)
-    dayPart = mo.group(4)
-    yearPart = mo.group(6)
-    afterPart = mo.group(8)
+    monthPart  = mo.group(2)
+    dayPart    = mo.group(4)
+    yearPart   = mo.group(6)
+    afterPart  = mo.group(8)
 
-    # form the new euro filename
+    # Form the European-style filename.
     euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
 
-    # get the full, absolute file paths
+    # Get the full, absolute file paths.
     absWorkingDir = os.path.abspath('.')
     amerFilename = os.path.join(absWorkingDir, amerFilename)
     euroFilename = os.path.join(absWorkingDir, euroFilename)
 
-    print('Renaming file %s to %s...' % (amerFilename,euroFilename))
-    # shutil.move(amerFilename, euroFilename)
+    # Rename the files.
+    print('Renaming "%s" to "%s"...\n' % (amerFilename, euroFilename))
+    #shutil.move(amerFilename, euroFilename) # uncomment after testing
